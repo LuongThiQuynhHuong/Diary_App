6a67b8098538987e44739ba17ae7cf0ca15c08d7
LuongThiQuynhHuong <holmestk24@gmail.com>
2021-11-09 22:01:48 +0700

    test 2

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableHighlight, Image } from 'react-native';
 
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(count + 1);
+
   return (
     <View style={styles.container}>
-      <TouchableHighlight>
-      <Image source = {require('../Diary_App/assets/back_btn.png')}/>
+      <TouchableHighlight style={styles.button}>
+          <Image source = {require("../Diary_App/assets/back_btn.png")}/>
       </TouchableHighlight>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -19,11 +21,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#bff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-  button: {
-    minHeight: 37,
-    minWidth: 37,
-    backgroundColor: '#fda'
+  button:{
+    height: 40,
+    width: 40,
+    backgroundColor: "#bf8"
   }
 });
